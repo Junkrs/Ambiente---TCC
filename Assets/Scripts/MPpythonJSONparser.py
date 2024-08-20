60cad0e835255dacbae0adb0763868599530d916
Gabriel Junqueira <57300618+Junkrs@users.noreply.github.com>
2024-08-20 19:04:32 -0300

    Fixed some stuff

--- Assets/Scripts/MPpythonJSONparser.py
+++ Assets/Scripts/MPpythonJSONparser.py
@@ -3,26 +3,30 @@ import json
 
 # Caminho do arquivo
 try:
+    # Desktop
+    # file_path = r"C:/Users/junqu/Documents/GitHub/Ambiente---TCC/Assets/Resources/JSON/abacaxi_articulador1.mp4_landmarks.json"
+    
+    # Notebook
     file_path = r"C:/Users/Gabriel Junqueira/Desktop/Unity Projects/Ambiente - TCC/Assets/Resources/JSON/abacaxi_articulador1.mp4_landmarks.json"
     with open(file_path, 'r') as file:
         data = json.load(file)
         UnityEngine.Debug.Log("JSON file loaded successfully!")
 except FileNotFoundError:
-     UnityEngine.Debug.Error("The JSON file was not found at the specified path")   
+     UnityEngine.Debug.LogError("The JSON file was not found at the specified path")   
 
 # Impressao do nome do video
 if 'nome_video' in data:
         video_name = data['nome_video']
         UnityEngine.Debug.Log(f"Nome do video: {video_name}")
 else:
-    UnityEngine.Debug.Log("'nome_video' not found in JSON data.")
+    UnityEngine.Debug.LogError("'nome_video' not found in JSON data.")
 
-# Iterate over each frame in the video
+# Iteracao em cada um dos frames do video
 for frame in data['landmarks_quadros']:
     for quadro_name, pontos in frame.items():
 
         UnityEngine.Debug.Log(f"Quadro: {quadro_name}")
-        # Iterate over each point in the frame
+        # Iteracao em cada um dos pontos
         for ponto in pontos:
             for ponto_name, coordinates in ponto.items():
                 x = coordinates['x']
